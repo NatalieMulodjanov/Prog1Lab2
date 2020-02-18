91a4d43dea3c885d539f995dec7acb0ea593d956
cstuser <ntaliemulodjanov@gmail.com>
2020-02-18 10:32:38 -0500

    added a scanner to get input from the user

---
+++ RectanglePerimeter.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class RectanglePerimeter {
+  public static void main (String[] args){
+
+    double length;
+    double width;
+    double perimeter;
+    double area;
+    
+    System.out.println("******RECTANGLE PERIMETER AND EREA CALCULATOR******");
+    
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Please enter the length of the rectangle");
+    length = sc.nextDouble(); 
+    
+    System.out.println("Please enter the width");
+    width = sc.nextDouble();
+ 
+    perimeter = 2 * (length + width);
+    area = length * width;
+
+    System.out.println("rectangle perimeter  " + perimeter +
+                        "\nrectangle area " + area);
+
+
+  }
+}
