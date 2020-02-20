65cf8046372374f239e89914cf80b60e6a5d2f53
Natalie Mulodjanov <ntaliemulodjanov@gmail.com>
2020-02-20 15:28:13 -0500

    changed the calculation of the minuted to a regular formula instead of an imported method

--- SecondsConverter.java
+++ SecondsConverter.java
@@ -1,27 +1,29 @@
-/*This is a program that will convert the input of seconds into minutes.
-the method used in this program is imported from the Duration class.
-the user input is in seconds and the output is in minutes. the method is
-deviding the seconds by 60s to get the amount of minutes.
+/*This is a program that will convert the amount of seconds input by the user
+to minutes. the user will enter any amount of seconds and the program will
+divide it to 60 seconds and will inout the minutes value in the following format:
+00:00.
 the program is written by Natalie Mulodjanov*/
 import java.util.*;// importing java packages
 import java.lang.*;
-import java.time.*;
+
 
 public class SecondsConverter{
   public static void main(String[] args) {
 
     int seconds;
     int minutes;
+    int rest;
     Scanner input = new Scanner(System.in);// imported from Scanner class
 
     System.out.println("\n\n\n*****SecondsConverter******\n\n\n");
 
-    System.out.println("Please enter the amount of seonds you'd like to" +
+    System.out.println("Please enter the amount of seconds you'd like to" +
                         " convert here: ");// user input
     seconds = input.nextInt();
 
-    minutes = Duration.toMinutes(seconds);// method from Duration class
-
-    System.out.println(seconds + "seconds are" + minutes + " minutes");
-  }
+    minutes = seconds / 60;
+    rest = seconds % 60;
+    System.out.println("The seconds you have entered equal to ");
+    System.out.println(minutes + ":" + rest + " minutes" );
+ }
 }
